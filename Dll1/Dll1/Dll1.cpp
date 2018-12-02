9bef44cb5151d598583fc02b6c20da76b116e890
Winielson <wm84@njit.edu>
2018-12-02 15:24:24 -0500

    test

---
+++ Dll1/Dll1/Dll1.cpp
@@ -0,0 +1,5 @@
+﻿// Dll1.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "pch.h"
+#include "Dll1.h"
---
+++ gamex86/gamex86/gamex86.cpp
@@ -0,0 +1,5 @@
+﻿// gamex86.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "pch.h"
+#include "gamex86.h"
